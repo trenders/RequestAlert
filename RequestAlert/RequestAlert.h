eded05faad26b240109b42ff2a0e5637c607752a
KEN-chan <hellokenta1024@gmail.com>
2016-03-07 19:13:21 +0900

    Initial Commit

---
+++ RequestAlert/RequestAlert.h
@@ -0,0 +1,19 @@
+//
+//  RequestAlert.h
+//  RequestAlert
+//
+//  Created by HARA KENTA on 2016/03/07.
+//  Copyright © 2016年 Hara Kenta. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RequestAlert.
+FOUNDATION_EXPORT double RequestAlertVersionNumber;
+
+//! Project version string for RequestAlert.
+FOUNDATION_EXPORT const unsigned char RequestAlertVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RequestAlert/PublicHeader.h>
+
+
