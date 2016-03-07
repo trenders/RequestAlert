59c5da2818b4be6b83021a52cac84e5f27a60d47
KEN-chan <hellokenta1024@gmail.com>
2016-03-07 19:33:07 +0900

    add Demo

---
+++ Demo/Demo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Demo
+//
+//  Created by HARA KENTA on 2016/03/07.
+//  Copyright © 2016年 Hara Kenta. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
