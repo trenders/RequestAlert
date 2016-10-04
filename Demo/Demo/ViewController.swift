a2779adb85e8b47d809de89c19a1bc55f30287ad
KEN-chan <hellokenta1024@gmail.com>
2016-10-04 12:38:52 +0900

    create Demo

--- Demo/Demo/ViewController.swift
+++ Demo/Demo/ViewController.swift
@@ -5,7 +5,9 @@
 //  Created by HARA KENTA on 2016/03/07.
 //  Copyright © 2016年 Hara Kenta. All rights reserved.
 //
+
 import UIKit
+import RequestAlert
 
 class ViewController: UIViewController {
 
@@ -13,12 +15,54 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    override func viewDidAppear(animated: Bool) {
+
+        super.viewDidAppear(animated)
+        
+        RequestAlert.showAlert(id: RequestAlertType.pushNotification.id) {
+            
+            print("push OK button of Request Alert of type of push notification")
+        }
+        
+        if RequestAlert.hasDisplayed(id: RequestAlertType.review.id) {
+            
+            print("review request alert has been displayed")
+        }
+        
+        if RequestAlert.hasBeenPushedOKButton(id: RequestAlertType.share.id) {
+            
+            print("share request alert has been pushed OK Button")
+        }
+    }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func pushedPushNotificationButton(sender: AnyObject) {
+        
+        RequestAlert.incrementCount(id: RequestAlertType.pushNotification.id) { 
+            
+            print("push OK button of Request Alert of type of push notification")
+        }
+    }
 
+    @IBAction func pushedReviewButton(sender: AnyObject) {
+        
+        RequestAlert.incrementCount(id: RequestAlertType.review.id) { 
+            
+            print("push OK button of Request Alert of type of review")
+        }
+    }
+
+    @IBAction func pushedShareButton(sender: AnyObject) {
+        
+        RequestAlert.incrementCount(id: RequestAlertType.share.id) { 
+            
+            print("push OK button of Request Alert of type of share")
+        }
+    }
 }
 
